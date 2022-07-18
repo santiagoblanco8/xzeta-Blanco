5d7ef8ec8bc6a19c18be8c02c9030eeebb70ff5c
santiagoblanco8 <89712717+santiagoblanco8@users.noreply.github.com>
2022-07-18 20:50:57 -0300

    componente Cart y cartcontext arreglado

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,91 @@
-import React, { useContext } from 'react'
-import { myContext } from './CartContext'
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useContext } from 'react';
+import { myContext } from './CartContext';
+import { Typography } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import "./Cart.css";
+import {Link} from 'react-router-dom';
+
+
+
+
+
 
 
 export default function Cart() {
-    const {cart, removeItem} = useContext(myContext)
-  return (<>
-  <div>
-    {cart.description}
-    {cart.title}
+    
+    const {cart, removeItem, clear, cartVacio, importe} = useContext(myContext);  
+  
+
+  return (
+    <>
+    <div className="divCart">
+      <Typography  style={{color:'#e0f193d7', textAlign:'center', margin:'2rem'}} variant="h4"> Carrito </Typography>
+
+      <TableContainer component={Paper} sx={{ width:8/10, borderRadius:'15px' }}>
+      <Table  aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell ></TableCell>
+            <TableCell sx={{ width:5/10,}} align="center">Producto</TableCell>
+            <TableCell align="center">Precio</TableCell>
+            <TableCell align="center">Subtotal</TableCell>
+            <TableCell align="center"></TableCell>
+            <TableCell align="center"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cart.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" >
+                <img  style={{width:'3rem'}} src={row.pictureUrl} alt={row.name}/>               
+              </TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="center">$ {row.price}</TableCell>
+              <TableCell align="center">$ {row.price*row.cant}</TableCell>
+              <TableCell align="center">{row.cant}</TableCell>
+              <TableCell align="center"><Button variant="contained" onClick={()=>{removeItem(row.id)}} > <DeleteForeverIcon/> </Button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+        <div className={cartVacio ? 'SeVe' : 'NoSeVe'}>
+          <div  style={{backgroundColor:'#283322', borderRadius:'10px', padding:'10px', display:'flex', flexDirection:'column', alignItems:'center'}}>
+            <Typography variant="h5" style={{color:'#e0f193d7', fontFamily:'Joan'}}> Tu carrito está vacío, te invitamos a conocer nuestros productos. </Typography>
+            <Button variant="contained"> <Link to={"/"}  style={{ color: 'inherit', textDecoration: 'none' }}> Comenzar a comprar</Link> </Button>
+          </div>
+        </div>
+        <div className={cartVacio ? 'NoSeVe' : 'SeVe'}>
+          <div className="divFinalizarCompra">
+            <Typography style={{color:'#e0f193d7', textAlign:'center'}} variant="h5">
+              Importe total  $ {importe}
+            </Typography>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained"> <Link to='/checkout' style={{ color: 'inherit', textDecoration: 'none' }}> Finalizar compra</Link></Button>
+              <Button variant="outlined" onClick={()=>{  clear()}}  sx={{ color:'secondary.main'}}> Vaciar carrito</Button>
+            </Stack>
+          </div>
+        </div>
+
     </div>
+
     </>
-  )
+  );
+
+
+    
 }
--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -6,36 +6,55 @@ export const myContext = createContext(null);
 
 export default function CartContext({children}) {
     const [cart, setCart] = useState([]);
+    const [cantidad, setCantidad] = useState(0);
+    const [importe, setImporte] = useState(0);
+    const [cartVacio, setCartVacio] = useState(true)
 
     useEffect (() => {
       console.log(cart)
     }, [cart])
 
     function addItem(item, cant) {
-      let alreadyWas = isInCart(item.id);
-      console.log(alreadyWas)
-      if(alreadyWas){
-        removeItem(alreadyWas.id);
-        setCart( [...cart, {...item, cant:cant +alreadyWas.cant}])
-      }else{
-        setCart([...cart, {...item, cant:cant}])
-      }
+      setCartVacio(false)
+      if(isInCart(item.id)){
+        let newCart = cart;
+        let indexProducto = newCart.findIndex(element=> element.id === item.id);
+        newCart[indexProducto].cant = Number(newCart[indexProducto].cant) + Number(cant);
+        setCart(newCart);
+        setCantidad(newCart.reduce((acc, element)=> acc + element.cant, 0));
+        setImporte(newCart.reduce((acc,elementX) => acc + elementX.price*elementX.cant, 0));
+
+      } else {
+        const newCartX = [...cart, {...item, cant:cant}];
+        setCart(newCartX);
+        setCantidad(newCartX.reduce((acc, element)=> acc + element.cant, 0));
+        setImporte(newCartX.reduce((acc, elementX)=> acc + elementX.price*elementX.cant, 0));
+        
     }
+  }
 
     function removeItem(itemId) {
-      setCart( cart.filter((este) => este.id !== itemId))
+      const newCartX =  cart.filter((este) => este.id !== itemId);
+      newCartX===[] && setCartVacio(true);
+      setCart(newCartX)
+      setCantidad(newCartX.reduce((acc, element)=> acc + element.cant, 0));
+      setImporte(newCartX.reduce((acc, elementX)=> acc + elementX.price*elementX.cant, 0));
     }
 
     function clear() {
-      setCart([])
+      const newCartX= [];
+      setCart(newCartX);
+      setCantidad(0);
+      setCartVacio(true);
+      setImporte(0);
     }
 
     const isInCart = (id) => {
-      return cart.some((este) => este.id === id)
+      return cart.find((este) => este.id === id)
     }
   return (
     <>
-    <myContext.Provider value={{cart, addItem, removeItem, clear}}>
+    <myContext.Provider value={{cart, cantidad, importe, cartVacio, addItem, removeItem, clear}}>
     {children}
     </myContext.Provider>
     </>
