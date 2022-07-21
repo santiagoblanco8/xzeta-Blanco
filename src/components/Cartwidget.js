c0c703de32881db93224d8db77ac38046c086c97
santiagoblanco8 <89712717+santiagoblanco8@users.noreply.github.com>
2022-07-21 20:27:59 -0300

    Cart widget con cantidad agregada

--- src/components/Cartwidget.js
+++ src/components/Cartwidget.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useContext } from 'react';
+import { myContext } from './CartContext';
+import {Link} from 'react-router-dom'
+import "./NavBar.css";
 
 export default function Cartwidget() {
+  const { cantidad } = useContext(myContext)
   return (
-    <div><ShoppingCartIcon/></div>
+    <Link to= {`/cart`} style={{ color: 'inherit', textDecoration: 'none'}}>
+    <div>
+      <ShoppingCartIcon/>
+      <div className={cantidad===0 ? 'Invisible' : 'Visible'}>
+        <p> {cantidad} </p>
+      </div>
+      </div>
+    </Link>
   )
 }
